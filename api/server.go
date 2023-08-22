3434b152c8a5b9e1386c540e7c8bdfa9e861c392
belhiibeng <belhi.romdona@gmail.com>
2023-08-22 15:03:16 +0700

    implements go-mock

---
+++ api/server.go
@@ -0,0 +1,23 @@
+package api
+
+import (
+	db "github.com/belhiibeng/simple-bank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  db.Store
+	router *gin.Engine
+}
+
+func NewServer(store db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	server.router = router
+	return server
+}
